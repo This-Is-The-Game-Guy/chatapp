d2afb9b6bcaf2c7e5cd0089e4ebe39b6f93f30bf
This-Is-The-Game-Guy <anay1.madaboinia@gmail.com>
2026-06-01 11:31:04 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+socketio = SocketIO(app)
+
+messages = []
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+@socketio.on("message")
+def handle_message(data):
+    messages.append(data)
+    emit("message", data, broadcast=True)
+
+if __name__ == "__main__":
+    socketio.run(app, debug=False)
