d57906e306fa6f5c1fbe637257b7700e4ddaec2a
This-Is-The-Game-Guy <anay1.madaboinia@gmail.com>
2026-06-01 11:56:30 -0500

    fix for railway

--- app.py
+++ app.py
@@ -16,4 +16,4 @@ def handle_message(data):
     emit("message", data, broadcast=True)
 
 if __name__ == "__main__":
-    socketio.run(app, debug=False)
+    socketio.run(app, host="0.0.0.0", port=5000, debug=False)
