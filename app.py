0850e5812bb9a4e8d36f277b005f7b53c7e20aaa
This-Is-The-Game-Guy <anay1.madaboinia@gmail.com>
2026-06-01 12:48:14 -0500

    fix port

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def handle_message(data):
     emit("message", data, broadcast=True)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 5000))
+    port = int(os.environ.get("PORT", 8000))
     socketio.run(app, host="0.0.0.0", port=port, debug=False, allow_unsafe_werkzeug=True)
