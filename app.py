92492d03dc5f82dbc0d646b8bfa2db5b73c5fea0
This-Is-The-Game-Guy <anay1.madaboinia@gmail.com>
2026-06-02 18:29:28 -0500

    read html file directly

--- app.py
+++ app.py
@@ -1,22 +1,19 @@
 # v2
-from flask import Flask, render_template
+from flask import Flask
 from flask_socketio import SocketIO, emit
 import os
-print("FILES:", os.listdir(os.path.dirname(__file__)))
-print("TEMPLATES:", os.path.exists(os.path.join(os.path.dirname(__file__), 'templates')))
 
-app = Flask(__name__, template_folder=os.path.join(os.path.dirname(__file__), 'templates'))
+app = Flask(__name__)
 socketio = SocketIO(app, cors_allowed_origins="*")
 
-messages = []
+HTML = open(os.path.join(os.path.dirname(__file__), 'templates', 'index.html')).read()
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return HTML
 
 @socketio.on("message")
 def handle_message(data):
-    messages.append(data)
     emit("message", data, broadcast=True)
 
 if __name__ == "__main__":
