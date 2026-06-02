426cc8e836e6f854105a6223ac25eef9f59a4fd3
This-Is-The-Game-Guy <anay1.madaboinia@gmail.com>
2026-06-02 11:53:24 -0500

    fix port to 8080

--- app.py
+++ app.py
@@ -17,5 +17,5 @@ def handle_message(data):
     emit("message", data, broadcast=True)
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     socketio.run(app, host="0.0.0.0", port=port, debug=False, allow_unsafe_werkzeug=True)
